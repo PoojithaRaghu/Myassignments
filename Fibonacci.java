d12178956dcf01b992ec14ed70f3ac96ced42813
PoojithaRaghu <r.poojitha1991@gmail.com>
2024-10-17 12:48:48 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,12 +4,11 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		
-		 int terms = 10; 
 	        int a = 0, b = 1;
 
 	        System.out.print("Fibonacci Series: ");
 
-	        for (int i = 1; i <= terms; i++) 
+	        for (int i = 1; i <= 10; i++) 
 	        {
 	            System.out.print(a + " ");
 	            int next = a + b;
