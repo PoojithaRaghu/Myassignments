b95a6a66fdfd840e22012ac4ed06b3670dddd6b1
PoojithaRaghu <r.poojitha1991@gmail.com>
2024-10-19 09:16:09 +0530

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class IsPrime {
+	
+	public static void main(String[] args) {
+		int n=13;
+		int count=0;
+		
+		for (int i= 2; i<=n; i++){
+			if (n%2==0) {
+				count=1;
+				break;
+         
+			}
+			}
+		if (count==0) {
+			System.out.println("It is a Prime number");
+			}
+		else {
+			System.out.println("It is not a Prime number");
+			
+         }
+		
+		}
+
+}
+	
+		
+
+		
+	
